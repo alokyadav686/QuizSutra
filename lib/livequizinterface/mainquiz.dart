8665f03086c12d6a47523b77cf9aceb9d0b9998c
alokyadav686 <alokkumaryadav2799@gmail.com>
2024-11-24 18:24:54 +0530

    localapi success on main quiz

--- lib/livequizinterface/mainquiz.dart
+++ lib/livequizinterface/mainquiz.dart
@@ -121,105 +121,114 @@ class _MainquizState extends State<Mainquiz> {
             
                 ),
                 SizedBox(height: 20,),
-            
-                 SizedBox(height: 40,),
-              
+                          
                   Container(
                     width: double.infinity,
                  
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: FutureBuilder(
+                       child: FutureBuilder(
+                    future: quiz, 
+                    builder: (BuildContext context , AsyncSnapshot snapshot){
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                            child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation(Colors.white)));
+                      }
+                      if( snapshot.hasData){
+          
+                        var quizData = snapshot.data["codingquiz"];
+                                //  print(quizData);
                         
-                        future: quiz, 
-                        builder: (BuildContext context , AsyncSnapshot snapshot){
-                          if (snapshot.connectionState == ConnectionState.waiting) {
-                            return Center(
-                                child: CircularProgressIndicator(
-                                    valueColor: AlwaysStoppedAnimation(Colors.white)));
-                          }
-                          if( snapshot.hasData){
-              
-                            var quizData = snapshot.data["codingquiz"];
-                                    //  print(quizData);
-                            
-                            var questions = quizData["questions"];
-                            var queskey = questions[currentQuestionindex]["quesKey"];
+                        var questions = quizData["questions"];
+                      
+                        if (selectedAnswers.length < questions.length) {
+                          selectedAnswers = List.generate(
+                              questions.length, (index) => '');
+                        }         
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
 
-                            if (selectedAnswers.length < questions.length) {
-                              selectedAnswers = List.generate(
-                                  questions.length, (index) => '');
-                            }
-                            
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                            
-                                 Text("Question ${queskey}",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600, color: Colors.white),),
-                                 SizedBox(height: 10,),
-                                          
-                                          Container(
-                                             width: double.infinity,
-                                             decoration: BoxDecoration(
-                                             border: Border.all(color: Colors.white),
-                                             color: Colors.black26
-                                              ),
-                                             child: Padding(
-                                           padding: const EdgeInsets.all(8.0),
-                                             child: Text(questions[currentQuestionindex]["question"],style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700,color: Colors.white),),
-                                              ),
-                                 ),
-                            
-                                      SizedBox(height: 40,),
-                                      
-                                      ListView.builder(
-                                        shrinkWrap: true,
-                                        itemCount: questions[currentQuestionindex]["options"].length,
-                                        itemBuilder: (BuildContext context, int index){
-                                          var option = questions[currentQuestionindex]["options"][index];
-                                           bool isSelected = selectedAnswers[currentQuestionindex] == option;
-            
-                                        
-                                          return Padding(
-                                            padding: const EdgeInsets.only(bottom: 10),
-                                            child: InkWell(
-                                              onTap: () {
-            
-                                                onOptionSelected(option);
-            
-                                                print("firstdebug ${option}");
-                                              },
-                                              child: Container(
-                                                     width: double.infinity,
-                                                                              
-                                                    decoration: BoxDecoration(
-                                                  color: isSelected ? Colors.black:Colors.black38,
-                                                  borderRadius: BorderRadius.circular(18)
-                                                   ),
-                                                  child: Center(child: Padding(
-                                              padding: const EdgeInsets.symmetric(vertical: 15),
-                                              child: Text(option ,style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.w500),),
-                                                )),
-                                               ),
-                                            ),
-                                          );
-                                        }                                
-                                        ),
-                                                                                               
-                              ],
-                            );
-                          }
-              
-                          else{
-              
-                            return Center( child:  CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation(Colors.white),
-                            ),);
-              
-                          }
-                        },
+                              Center(child: Text("${quizData["quizTitle"]}",style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),)),
+                              SizedBox(height: 20,),
                         
-                        )
+                             Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                               children: [
+                                 Text("Question ${currentQuestionindex +1}",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600, color: Colors.white),),
+                                 Text("Category : ${questions[currentQuestionindex]["category"]}",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600, color: Colors.white),),
+                               ],
+                             ),
+                             SizedBox(height: 10,),
+                                      
+                                      Container(
+                                                                            width: double.infinity,
+                                                                            decoration: BoxDecoration(
+                                                                             border: Border.all(color: Colors.white),
+                                                                             color: Colors.black26
+                                                                             // borderRadius: BorderRadius.circular(8),
+                                                                             // boxShadow: [BoxShadow(color: Colors.black26,)],
+                                                                            ),
+                                                                           child: Padding(
+                                                                             padding: const EdgeInsets.all(8.0),
+                                                                             child: Text(questions[currentQuestionindex]["questionText"],style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700,color: Colors.white),),
+                                                                           ),
+                                                                          ),
+                        
+                                  SizedBox(height: 40,),
+                                  
+                                  ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: questions[currentQuestionindex]["options"].length,
+                                    itemBuilder: (BuildContext context, int index){
+                                      var option = questions[currentQuestionindex]["options"][index];
+                                       bool isSelected = selectedAnswers[currentQuestionindex] == option;
+      
+                                    
+                                      return Padding(
+                                        padding: const EdgeInsets.only(bottom: 10),
+                                        child: InkWell(
+                                          onTap: () {
+      
+                                            onOptionSelected(option);
+      
+                                            print("firstdebug ${option}");
+                                          },
+                                          child: Container(
+                                                 width: double.infinity,
+                                                                          
+                                                decoration: BoxDecoration(
+                                               
+                                              // color:optiontapped?  Color.fromARGB(255, 114, 20, 131): Colors.purple[50],
+                                              // color: const Color.fromARGB(255, 30, 29, 30),
+                                              color: isSelected ? Colors.black:Colors.black38,
+                                              borderRadius: BorderRadius.circular(18)
+                                               ),
+                                              child: Center(child: Padding(
+                                          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                                          child: Text(option ,style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.w500),),
+                                            )),
+                                           ),
+                                        ),
+                                      );
+                                    }                                
+                                    ),
+                                                                                           
+                          ],
+                        );
+                      }
+          
+                      else{
+          
+                        return Center( child:  CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ),);
+          
+                      }
+                    },
+                    
+                    )
                     ),
                   ),
                   SizedBox(height: 20,),
